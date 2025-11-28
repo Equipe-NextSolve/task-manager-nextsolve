8a68e9174c276240e6960a4b57bba4e8966a258e
Dev-LDRC <lucasrabelocruz@gmail.com>
2025-11-28 10:23:45 -0300

    updating nextjs project setup - following the documentation

---
+++ src/app/login/page.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-2 min-h-screen items-center justify-center text-app-secundary-white">
+      <h1 className="text-5xl">Login</h1>
+      <h2 className="text-xl">/login</h2>
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-2 min-h-screen items-center justify-center text-app-secundary-white">
+      <h1 className="text-5xl">Home</h1>
+      <h2 className="text-xl">/</h2>
+    </div>
+  );
+}
